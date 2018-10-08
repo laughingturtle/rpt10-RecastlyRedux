1a4157851e369cde23831d2600f7ce40c08cbc90
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-10-08 11:46:50 -0700

    Complete planning state

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -6,6 +6,7 @@ var rootReducer = () => {};
 
 //TODO: define the root reducer for this app
 
+
 //HINT: you'll need to combine the other two reducers in this
 //  app into a single reducer using the 'combineReducers' method
 //  listed above.
